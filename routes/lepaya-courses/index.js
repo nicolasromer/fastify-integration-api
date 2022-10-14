90a87b42668912851590d9bb46c888f9a328f747
nicolasromer <nicolasromer@gmail.com>
2022-10-14 11:45:35 +0200

    master: add course endpoint with real data test

--- routes/lepaya-courses/index.js
+++ routes/lepaya-courses/index.js
@@ -1,7 +1,47 @@
 'use strict'
 
+const fetch = require("node-fetch");
+
+// module.exports = async function (fastify, opts) {
+//   fastify.get('/', async function (request, reply) {
+//     return 'Lepaya course api'
+//   })
+// }
+
 module.exports = async function (fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    return 'foo'
+  fastify.get('/:courseUid', async function (request, reply) {
+    const { courseUid } = request.params;
+
+    const baseUrl = 'https://kbfszrxx5vacidgrgdhqzu25r40vyyuw.lambda-url.eu-central-1.on.aws/api';
+    
+    const courseUrl = baseUrl + '/courses/' + courseUid;
+    const course = await fetch(courseUrl)
+      .then(response => response.json());
+
+   
+    const { trainerId, learners } = course;
+
+    const trainerUrl = baseUrl + '/trainers/' + trainerId;
+    const trainerPromise = fetch(trainerUrl)
+      .then(response => response.json());
+
+    const learnersUrl = baseUrl + '/learners';
+    const learnersPromise = fetch(learnersUrl)
+      .then(response => response.json());
+
+    const [trainerData, learnersData] = await Promise.all([trainerPromise, learnersPromise]);
+
+    const relevantLearners = learnersData.learners.filter(learner => course.learners.includes(learner.id) )
+
+
+    const {id, title, date} = course;
+
+    const payload = {
+      id, title, date,
+      trainer: trainerData,
+      learners: relevantLearners,
+    };
+
+    reply.send(payload);
   })
 }
--- test/routes/lepaya-courses.test.js
+++ test/routes/lepaya-courses.test.js
@@ -3,11 +3,58 @@
 const { test } = require('tap')
 const { build } = require('../helper')
 
-test('lepaya-courses root is loaded', async (t) => {
+// test('lepaya-courses root is loaded', async (t) => {
+//   const app = await build(t)
+
+//   const res = await app.inject({
+//     url: '/api/lepaya-courses'
+//   })
+//   t.equal(res.payload, 'Lepaya course api')
+// })
+
+test('can fetch a course', async (t) => {
   const app = await build(t)
 
   const res = await app.inject({
-    url: '/api/lepaya-courses'
+    url: '/api/lepaya-courses/ef131a0c-3006-4a38-8cfd-085fa08f8361'
   })
-  t.equal(res.payload, 'Lepaya course api')
+
+  t.equal(
+    res.payload, 
+    JSON.stringify({
+      "id": "ef131a0c-3006-4a38-8cfd-085fa08f8361",
+      "title": "Business-focused bifurcated secured line",
+      "date": "2073-01-18T06:57:06.870Z",
+      "trainer": {
+        "id": "d2b16518-cb01-42b1-970a-a5337dffc155",
+        "name": "Miss Linda Conroy"
+      },
+      "learners": [
+        {
+          "id": "d7aaf22e-d82f-448e-bc09-a6072e788b20",
+          "name": "Manuel Casper"
+        },
+        {
+          "id": "e9c7a7cd-33d5-4007-a6a4-11e9c208eb76",
+          "name": "Kathleen Conroy"
+        },
+        {
+          "id": "b98509fc-a393-4e43-9bf3-32f506ec7fa0",
+          "name": "Mrs. Johanna Lebsack"
+        },
+        {
+          "id": "8ec62b73-d69d-43eb-b3c9-3b71aad259de",
+          "name": "Patrick Mills"
+        },
+        {
+          "id": "3988158d-7309-46c1-bec1-8cb21b50f7b4",
+          "name": "Rudy Walker"
+        },
+        {
+          "id": "a259bc34-01c8-4452-89a1-66a36a01cc7b",
+          "name": "Ernesto Wiza"
+        }
+      ]
+    })
+  )
 })
